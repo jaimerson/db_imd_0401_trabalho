becdc884de757d72f97332239b8c651ead967ced
James <jaimersonaraujo@gmail.com>
2017-09-22 16:42:02 -0300

    Fix table appearance

--- db/queries/products_and_workers.sql
+++ db/queries/products_and_workers.sql
@@ -1,2 +1,4 @@
-SELECT descricao, area_plantada, nome as nome_trabalhador, especialidade as especialidade_trabalhador
-  FROM produto INNER JOIN trabalhador USING(id_trabalhador);
+SELECT descricao, trunc(area_plantada, 2),
+  nome as nome_trabalhador,
+  especialidade as especialidade_trabalhador
+FROM produto INNER JOIN trabalhador USING(id_trabalhador);
--- db/queries/sold_products.sql
+++ db/queries/sold_products.sql
@@ -1,5 +1,6 @@
-SELECT descricao, nome as nome_trabalhador, especialidade as especialidade_trabalhador
-  FROM produto
-    INNER JOIN venda USING(id_produto)
-    INNER JOIN trabalhador USING(id_trabalhador)
-  GROUP BY id_produto, nome_trabalhador, especialidade_trabalhador;
+SELECT descricao, nome as nome_trabalhador,
+  especialidade as especialidade_trabalhador
+FROM produto
+  INNER JOIN venda USING(id_produto)
+  INNER JOIN trabalhador USING(id_trabalhador)
+GROUP BY id_produto, nome_trabalhador, especialidade_trabalhador;
