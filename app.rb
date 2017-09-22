0f83347f0f17d8ba2b587ab78b53a6417ecdb2c0
James <jaimersonaraujo@gmail.com>
2017-09-22 12:31:30 -0300

    Add first report

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
 require_relative 'setup'
 require 'sinatra'
 
+class Report
+  class << self
+    class Item < Struct.new(:identifier, :query)
+      def execute(connection)
+        connection.fetch(query).map(&:itself)
+      end
+    end
+
+    attr_accessor :items
+
+    def item(identifier)
+      query = File.read("db/queries/#{identifier}.sql")
+      self.items ||= []
+      self.items << Item.new(identifier, query)
+    end
+  end
+
+  item 'sales_between_50_and_200'
+
+  attr_reader :connection
+
+  def initialize(connection)
+    @connection = connection
+  end
+
+  def items
+    self.class.items.reduce({}) do |result, item|
+      result[item.identifier] = item.execute(connection)
+      result
+    end
+  end
+
+end
+
 get '/' do
-  'Hello'
+  connection = Sequel.connect(database_url)
+  report = Report.new(connection)
+  erb :index, locals: { report: report }
 end
---
+++ db/queries/sales_between_50_and_200.sql
@@ -0,0 +1,5 @@
+SELECT id_produto, c.nome_comprador, quantidade, valor
+  FROM venda
+  INNER JOIN comprador c USING(id_comprador)
+  WHERE quantidade BETWEEN 50 AND 200
+    AND valor < 60000::money;
